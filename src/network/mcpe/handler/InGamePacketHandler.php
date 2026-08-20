46ad74fa4dc97d248cc5e015392299ea75a53712
xRookieFight <me@xrookiefight.com>
2026-08-20 18:10:02 +0300

    Bedrock 1.26.45

--- src/network/mcpe/handler/InGamePacketHandler.php
+++ src/network/mcpe/handler/InGamePacketHandler.php
@@ -333,9 +333,6 @@ class InGamePacketHandler extends PacketHandler{
 		$result = true;
 
 		$trData = $packet->trData;
-		if($trData === null){
-			throw new PacketHandlingException("Inventory transaction data is missing");
-		}
 
 		if(count($trData->getActions()) > 50){
 			throw new PacketHandlingException("Too many actions in inventory transaction");

3e9d44c590fd7dc3d39e1df1c3dfa9228813513b
xRookieFight <me@xrookiefight.com>
2026-09-02 20:45:04 +0300

    fix: guard against negative book page numbers

--- src/network/mcpe/handler/InGamePacketHandler.php
+++ src/network/mcpe/handler/InGamePacketHandler.php
@@ -1124,6 +1124,9 @@ class InGamePacketHandler extends PacketHandler{
 		switch($packet->type){
 			case BookEditPacket::TYPE_REPLACE_PAGE:
 				$text = self::checkBookText($packet->text, "page text", self::PAGE_LENGTH_SOFT_LIMIT_CHARS, WritableBookPage::PAGE_LENGTH_HARD_LIMIT_BYTES, $cancel);
+				if($packet->pageNumber < 0){
+					throw new PacketHandlingException("Page number cannot be negative");
+				}
 				$newBook->setPageText($packet->pageNumber, $text);
 				$modifiedPages[] = $packet->pageNumber;
 				break;
@@ -1145,6 +1148,9 @@ class InGamePacketHandler extends PacketHandler{
 				$modifiedPages[] = $packet->pageNumber;
 				break;
 			case BookEditPacket::TYPE_SWAP_PAGES:
+				if($packet->pageNumber < 0 || $packet->secondaryPageNumber < 0){
+					throw new PacketHandlingException("Page numbers cannot be negative");
+				}
 				if(!$newBook->pageExists($packet->pageNumber) || !$newBook->pageExists($packet->secondaryPageNumber)){
 					//the client will create pages on its own without telling us until it tries to switch them
 					$newBook->addPage(max($packet->pageNumber, $packet->secondaryPageNumber));
